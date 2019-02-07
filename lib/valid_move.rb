2a3a582acacf611a4a816303ae8055a53287df69
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 20:52:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 
 
 def valid_move(board,index)
+  index.between??()
 end 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 def position_taken?(board, index)
